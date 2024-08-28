620ef5615c61cf1428f6e693361557294dfe027d
Niels Koster <n.koster@portavita.eu>
2024-08-29 00:35:05 +0200

    Implement responsive hamburger menu

--- static/js/helper.js
+++ static/js/helper.js
@@ -1,7 +1,15 @@
 function highlightActiveLink() {
   const navLinks = document.querySelectorAll('header nav a');
+  const hamburgerLinks = document.querySelectorAll('#hamburger-menu a');
   const currentURL = window.location.pathname;
   navLinks.forEach(link => {
+    if (link.getAttribute('href') === currentURL) {
+      link.classList.add('active');
+    } else {
+      link.classList.remove('active');
+    }
+  });
+  hamburgerLinks.forEach(link => {
     if (link.getAttribute('href') === currentURL) {
       link.classList.add('active');
     } else {
@@ -12,3 +20,17 @@ function highlightActiveLink() {
 
 document.addEventListener("DOMContentLoaded", highlightActiveLink);
 document.addEventListener("htmx:afterSwap", highlightActiveLink);
+
+document.addEventListener('DOMContentLoaded', function() {
+  const toggle = document.getElementById('toggle');
+  const menu = document.getElementById('hamburger-menu');
+
+  toggle.addEventListener('change', function() {
+    if (toggle.checked) {
+      menu.style.minHeight = '13rem';
+    } else {
+      menu.style.minHeight = '0';
+    }
+  });
+
+});
