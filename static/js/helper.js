4f489eb5b0a3008f2a4d4c129a712b52d0a49ca9
Niels Koster <n.koster@portavita.eu>
2024-08-29 17:58:12 +0200

    Improve hamburger, workaround for resize bug (reload page)

--- static/js/helper.js
+++ static/js/helper.js
@@ -23,14 +23,30 @@ document.addEventListener("htmx:afterSwap", highlightActiveLink);
 
 document.addEventListener('DOMContentLoaded', function() {
   const toggle = document.getElementById('toggle');
+  const hamburger = document.querySelector('.hamburger');
   const menu = document.getElementById('hamburger-menu');
 
   toggle.addEventListener('change', function() {
     if (toggle.checked) {
       menu.style.minHeight = '13rem';
+      hamburger.style.rotate = '-90deg';
     } else {
       menu.style.minHeight = '0';
+      hamburger.style.rotate = '0deg';
     }
   });
+
+});
+
+window.myLoadingCheck = false;
+window.addEventListener('resize', function() {
+  if (!myLoadingCheck) {
+    window.myLoadingCheck = true;
+    setTimeout(() => {
+      console.log('reload');
+      window.location.reload();
+      window.myLoadingCheck = false;
+    }, 1000);
+  }
 
 });
